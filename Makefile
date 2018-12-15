9a972157175bbce0bafa0b126467b20b15733da0
Trammell hudson <hudson@trmm.net>
2018-12-15 14:10:43 -0500

    serial port demo works!

--- Makefile
+++ Makefile
@@ -2,7 +2,15 @@
 all: pulse.bin
 
 pulse.blif: pulse.v
-serial.blif: serial.v
+serial.blif: serial.v uart.v pll_96.v
+
+pll_96.v:
+	icepll \
+		-i 48 \
+		-o 96 \
+		-m \
+		-n pll_96 \
+		-f pll_96.v \
 
 
 include Makefile.icestorm
--- serial.v
+++ serial.v
@@ -9,24 +9,82 @@ module top(
 	output led_g,
 	output led_b,
 	output serial_txd,
-	input serial_rxd
+	input serial_rxd,
+	output debug0
 );
-	wire clk_48;
+	wire clk_48, clk_96, locked;
+	wire reset = !locked;
 	SB_HFOSC u_hfosc (
 		.CLKHFPU(1'b1),
 		.CLKHFEN(1'b1),
 		.CLKHF(clk_48)
 	);
 
+	pll_96 pll(
+		.clock_in(clk_48),
+		.clock_out(clk_96),
+		.locked(locked)
+	);
+
 	reg [31:0] counter;
 
 	always @(posedge clk_48)
-		counter <= counter + 1;
+		if (reset)
+			counter <= 0;
+		else
+			counter <= counter + 1;
 
-	assign led_r = 1;
 	assign led_g = 1;
 	assign led_b = serial_rxd; // idles high
 
+	// generate a 3 MHz serial clock from the 96 MHz clock
+	reg [4:0] counter_96;
+	wire clk_3;
+	assign clk_3 = (counter_96 == 5'b0);
+	always @(posedge clk_96)
+		counter_96 <= counter_96 + 1;
 
+	reg [7:0] uart_txd;
+	reg uart_txd_strobe;
+	wire uart_txd_ready;
+
+	uart_tx txd(
+		.mclk(clk_96),
+		.reset(reset),
+		.baud_x1(clk_3),
+		.serial(serial_txd),
+		.ready(uart_txd_ready),
+		.data(uart_txd),
+		.data_strobe(uart_txd_strobe)
+	);
+
+	assign debug0 = serial_txd;
+	reg [3:0] byte_counter;
+
+	always @(posedge clk_48) begin
+		led_r <= 1;
+
+		if (reset) begin
+			// nothing
+			byte_counter <= 0;
+			uart_txd_strobe <= 0;
+		end else
+		if (uart_txd_ready && !uart_txd_strobe && counter[20:0] == 0) begin
+			// ready to send a new byte
+			uart_txd_strobe <= 1;
+
+			if (byte_counter == 0)
+				uart_txd <= "\r";
+			else
+			if (byte_counter == 1)
+				uart_txd <= "\n";
+			else
+				uart_txd <= 8'h41 + byte_counter - 2;
+			byte_counter <= byte_counter + 1;
+			led_r <= 0;
+		end else begin
+			uart_txd_strobe <= 0;
+		end
+	end
 endmodule
 
