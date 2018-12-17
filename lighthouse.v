be20cc60a496bb31c1f4889db241e1055430c9cf
Trammell hudson <hudson@trmm.net>
2018-12-16 23:21:10 -0500

    lighthouse edge detection seems to work

--- lighthouse.v
+++ lighthouse.v
@@ -1,6 +1,8 @@
 /** \file
  * Print the lengths of timer pulses from the lighthouse sensors.
  */
+`include "util.v"
+`include "uart.v"
 
 module top(
 	output led_r,
@@ -9,9 +11,15 @@ module top(
 	output serial_txd,
 	input serial_rxd,
 	output spi_cs,
-	output debug0
+	output debug0,
+	input gpio_18,
+	input gpio_28,
+	input gpio_38
 );
 	assign spi_cs = 1; // it is necessary to turn off the SPI flash chip
+
+	// map the sensor
+	wire lighthouse_a = gpio_28;
 
 	wire clk_48;
 	wire reset = 0;
@@ -64,43 +72,64 @@ module top(
 		.data_strobe(uart_txd_strobe)
 	);
 
-function [7:0] hexdigit;
-	input [3:0] x;
-	begin
-		hexdigit =
-			x == 0 ? "0" :
-			x == 1 ? "1" :
-			x == 2 ? "2" :
-			x == 3 ? "3" :
-			x == 4 ? "4" :
-			x == 5 ? "5" :
-			x == 6 ? "6" :
-			x == 7 ? "7" :
-			x == 8 ? "8" :
-			x == 9 ? "9" :
-			x == 10 ? "a" :
-			x == 11 ? "b" :
-			x == 12 ? "c" :
-			x == 13 ? "d" :
-			x == 14 ? "e" :
-			x == 15 ? "f" :
-			"?";
-	end
-endfunction
+	// output buffer
+	reg [31:0] timer_fifo_write;
+	reg timer_fifo_write_strobe;
+	wire timer_fifo_available;
+	wire [31:0] timer_fifo_read;
+	reg timer_fifo_read_strobe;
 
-	reg [32:0] out;
-	reg [4:0] out_bytes;
+	fifo #(.WIDTH(32),.NUM(256)) timer_fifo(
+		.clk(clk_48),
+		.reset(reset),
+		.data_available(timer_fifo_available),
+		.write_data(timer_fifo_write),
+		.write_strobe(timer_fifo_write_strobe),
+		.read_data(timer_fifo_read),
+		.read_strobe(timer_fifo_read_strobe)
+	);
+
+	// timer delta
+	wire rise_strobe_a;
+	wire fall_strobe_a;
+	wire [23:0] length_a;
+
+	edge_capture edge_a(
+		.clk(clk_48),
+		.reset(reset),
+		.raw_pin(lighthouse_a),
+		.rise_strobe(rise_strobe_a),
+		.fall_strobe(fall_strobe_a),
+		.pulse_length(length_a)
+	);
+
+	always @(posedge clk_48)
+	begin
+		timer_fifo_write_strobe <= 0;
+
+		if (fall_strobe_a)
+		begin
+			timer_fifo_write <= { 8'h00, length_a };
+			timer_fifo_write_strobe <= 1;
+		end else
+		if (rise_strobe_a)
+		begin
+			timer_fifo_write <= { 8'h80, length_a };
+			timer_fifo_write_strobe <= 1;
+		end
+	end
+
+	reg [31:0] out = 32'hDECAFBAD;
+	reg [4:0] out_bytes = 10;
 
 	always @(posedge clk_48)
 	begin
 		uart_txd_strobe <= 0;
-		if (counter[15:0] == 0)
-		begin
-			out <= counter;
-			out_bytes <= 10;
-		end
+		timer_fifo_read_strobe <= 0;
 
-		if (out_bytes != 0) begin
+		// convert timer deltas to hex digits
+		if (out_bytes != 0)
+		begin
 			uart_txd_strobe <= 1;
 			out_bytes <= out_bytes - 1;
 			if (out_bytes == 1)
@@ -108,11 +137,62 @@ endfunction
 			else
 			if (out_bytes == 2)
 				uart_txd <= "\n";
+			else if (out_bytes == 10)
+				uart_txd <= out[31] ? "+" : "-";
 			else begin
 				uart_txd <= hexdigit(out[31:28]);
-				out <= { out[27:0], 4'b0 };
 			end
+
+			out <= { out[27:0], 4'b0 };
+		end else
+		if (timer_fifo_available)
+		begin
+			out <= timer_fifo_read;
+			timer_fifo_read_strobe <= 1;
+			out_bytes <= 10;
 		end
+	end
+
+endmodule
+
+
+module edge_capture(
+	input clk,
+	input reset,
+	input raw_pin,
+	output [WIDTH-1:0] pulse_length,
+	output rise_strobe,
+	output fall_strobe
+);
+	parameter WIDTH = 24;
+
+	reg [WIDTH-1:0] counter;
+	reg pin0;
+	reg pin1;
+	reg pin2;
+
+	always @(posedge clk)
+	begin
+		// defaults
+		rise_strobe <= 0;
+		fall_strobe <= 0;
+
+		// flop the raw pin the ensure stability
+		pin0 <= raw_pin;
+		pin1 <= pin0;
+		pin2 <= pin1;
+
+		if (reset)
+			counter <= 0;
+		else
+		if (pin2 != pin1) begin
+			rise_strobe <=  pin2;
+			fall_strobe <= !pin2;
+			pulse_length <= counter;
+			counter <= 0;
+		end else
+		if (counter != ~0)
+			counter <= counter + 1;
 	end
 
 endmodule
