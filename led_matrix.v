95cecd25b8a2e8638f4d320ee029d954b6ca4cd9
Trammell hudson <hudson@trmm.net>
2019-02-17 00:49:05 -0500

    uart to led works

--- led_matrix.v
+++ led_matrix.v
@@ -1,5 +1,12 @@
+`include "util.v"
+`include "uart.v"
+
 module top(
+	output serial_txd,
+	input serial_rxd,
+	output spi_cs,
 	output led_r,
+
 	output gpio_26,
 	output gpio_27,
 	output gpio_32,
@@ -13,16 +20,28 @@ module top(
 	output gpio_38,
 	output gpio_28
 );
-	wire reset = 0;
+	assign spi_cs = 1; // it is necessary to turn off the SPI flash chip
+	reg reset = 0;
 	wire clk_48mhz;
 	SB_HFOSC osc(1,1, clk_48mhz);
-	reg [8:0] counter;
-	always @(posedge clk_48mhz) counter <= counter + 1;
+	reg [10:0] counter;
+	always @(posedge clk_48mhz) begin
+		counter <= counter + 1;
+		if (~counter == 0)
+			reset <= 0;
+	end
+
 	wire clk = clk_48mhz; // counter[0];
 	
 
 	wire [2:0] led_addr = {gpio_36, gpio_34, gpio_43};
-	assign led_r = 1;
+
+	reg [7:0] r;
+	reg [7:0] g;
+	reg [7:0] b;
+	reg [7:0] x;
+	reg [7:0] y;
+	reg input_strobe;
 	
 
 	led_matrix matrix(
@@ -39,11 +58,77 @@ module top(
 		.led_b({gpio_37, gpio_32}),
 
 		//
-		.input_clk(0),
-		.strobe(0)
+		.input_clk(clk),
+		.strobe(input_strobe),
+		.r(r),
+		.g(g),
+		.b(b),
+		.x(x),
+		.y(y)
 	);
 
+	// generate a 3 MHz/12 MHz serial clock from the 48 MHz clock
+	// this is the 3 Mb/s maximum supported by the FTDI chip
+	wire clk_4;
+	divide_by_n #(.N( 4)) div4(clk_48mhz, reset, clk_4);
+
+	wire [7:0] uart_rxd;
+	wire uart_rxd_strobe;
+	assign serial_txd = 1;
+
+	uart_rx rxd(
+		.mclk(clk),
+		.reset(reset),
+		.baud_x4(clk_4),
+		.serial(serial_rxd),
+		.data(uart_rxd),
+		.data_strobe(uart_rxd_strobe)
+	);
+
+	reg [1:0] channel = 0;
+
+	always @(posedge clk)
+	begin
+		input_strobe <= 0;
+		led_r <= 1;
+
+		if (!uart_rxd_strobe)
+		begin
+			// nothing
+		end else
+		if (channel == 0) begin
+			if (x != 31)
+				x <= x + 1;
+			else begin
+				x <= 0;
+				if (y != 15)
+					y <= y + 1;
+				else
+					y <= 0;
+			end
+
+			r <= uart_rxd;
+			channel <= 1;
+			//led_r <= 0;
+		end else
+		if (channel == 1) begin
+			//led_r <= 0;
+			g <= uart_rxd;
+			channel <= 2;
+		end else
+		if (channel == 2) begin
+			//led_r <= 0;
+			b <= uart_rxd;
+			input_strobe <= 1;
+			channel <= 0;
+		end else begin
+			led_r <= 0;
+			channel <= 0;
+		end
+	end
+
 endmodule
+
 
 
 /*
@@ -77,6 +162,7 @@ module led_matrix(
 	parameter Y_SHIFT = 5; // CLOG2(X_RES)
 	parameter Y_STRIDE = 8; //1 << ADDR_WIDTH;
 	parameter Y_RES = ROWS * Y_STRIDE;
+	parameter DIM = 0;
 
 	reg [7:0] bright;
 	reg [7:0] led_x;
@@ -89,14 +175,22 @@ module led_matrix(
 	reg [7:0] framebuffer_g[X_RES * Y_RES - 1 : 0];
 	reg [7:0] framebuffer_b[X_RES * Y_RES - 1 : 0];
 
-	reg [15:0] pix_offset = (led_y << Y_SHIFT) | led_x;
-	wire [7:0] pix_r = framebuffer_r[pix_offset];
-	wire [7:0] pix_g = framebuffer_g[pix_offset];
-	wire [7:0] pix_b = framebuffer_b[pix_offset];
+	wire [15:0] pix_offset = (led_y << Y_SHIFT) | led_x;
+	reg [7:0] pix_r; // = framebuffer_r[pix_offset];
+	reg [7:0] pix_g; // = framebuffer_g[pix_offset];
+	reg [7:0] pix_b; // = framebuffer_b[pix_offset];
+
+	always @(posedge clk) begin
+		pix_r <= framebuffer_r[pix_offset];
+	 	pix_g <= framebuffer_g[pix_offset];
+		pix_b <= framebuffer_b[pix_offset];
+	end
 
 	initial $readmemh("red.hex", framebuffer_r);
 	initial $readmemh("green.hex", framebuffer_g);
 	initial $readmemh("blue.hex", framebuffer_b);
+
+	reg stall;
 
 	// output logic
 	always @(posedge clk)
@@ -114,6 +208,9 @@ module led_matrix(
 			led_oe <= 1;
 			all_rows_done <= 0;
 			all_pixels_done <= 0;
+		end else
+		if (stall) begin
+			stall <= 0;
 		end else
 		if (all_pixels_done)
 		begin
@@ -138,6 +235,7 @@ module led_matrix(
 				led_x <= 0;
 				all_pixels_done <= 0;
 				all_rows_done <= 0;
+				stall <= 1;
 			end
 		end else
 		if (all_rows_done)
@@ -159,15 +257,17 @@ module led_matrix(
 			end
 		end else begin
 			// update the output bit for this r/g/b
-			led_r[row] <= (pix_r >> 1) > bright;
-			led_g[row] <= (pix_g >> 1) > bright;
-			led_b[row] <= (pix_b >> 1) > bright;
+			led_r[row] <= (pix_r >> DIM) > bright;
+			led_g[row] <= (pix_g >> DIM) > bright;
+			led_b[row] <= (pix_b >> DIM) > bright;
 
 			//led_r[row] <= (led_x << 2) > bright;
 			//led_g[row] <= 0; //
 			//led_b[row] <= 0; //bright < 200;
 
 			led_y <= led_y + Y_STRIDE;
+			stall <= 1;
+
 			if (row == ROWS-1)
 				all_rows_done <= 1;
 			else
@@ -175,22 +275,19 @@ module led_matrix(
 		end
 	end
 
-`ifdef 0
 
 	// input can run in a separate clock domain
 	// this might need an input fifo to allow the framebuffer
 	// to move into an spram
+	wire [15:0] input_offset = (y << Y_SHIFT) | x;
+
 	always @(posedge input_clk)
 	begin
-		if (reset) begin
-			// nothing to do
-		end else
 		if (strobe) begin
-			framebuffer_r[(y << Y_SHIFT) | x] <= r;
-			framebuffer_g[(y << Y_SHIFT) | x] <= g;
-			framebuffer_b[(y << Y_SHIFT) | x] <= b;
+			framebuffer_r[input_offset] <= r;
+			framebuffer_g[input_offset] <= g;
+			framebuffer_b[input_offset] <= b;
 		end
 	end
-`endif
 
 endmodule
